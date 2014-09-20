3d6d6fb7d90ca32305b54eaffa281906d78b297b
JG Emmanuel <jg.emmanuel@outlook.com>
2014-09-20 18:39:39 +0530

    emacs file template support

--- emacs.d/init-templates.el
+++ emacs.d/init-templates.el
@@ -1,10 +1,14 @@
 
-;; org-mode templates
-(add-hook 'find-file-hooks 'load-template)
-(defun load-template ()
+(add-hook 'find-file-hooks 'load-templates)
+(defun load-templates ()
   (interactive)
+  ;; org-mode templates
   (when (and
-         ;; (string-match "\\.org$" (buffer-file-name))
          (string-match "notes.org$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file "~/.emacs.d/templates/notes.org")))
+    (insert-file "~/.emacs.d/templates/notes.org"))
+  ;; test template
+  (when (and
+         (string-match "\\.test$" (buffer-file-name))
+         (eq 1 (point-max)))
+    (insert-file "~/.emacs.d/templates/test.test")))
